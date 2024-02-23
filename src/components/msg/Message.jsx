d2e15258038e03a2ca4370916d2304b1b9bf357e
Rakyun <kiwipower125@gmail.com>
2024-02-23 17:54:22 +0700

    Added Messenger

--- src/components/msg/Message.jsx
+++ src/components/msg/Message.jsx
@@ -5,7 +5,7 @@ const Message = () => {
     <>
       <a
         href="https://www.facebook.com/messages/t/102949192458304"
-        className="fixed bottom-4 right-10 flex h-14 w-14 cursor-pointer items-center justify-center rounded-full bg-[#F7C94B] hover:opacity-90 active:opacity-80"
+        className="fixed bottom-4 right-10 flex h-14 w-14 animate-bounce cursor-pointer items-center justify-center rounded-full bg-[#F7C94B] hover:opacity-90 active:opacity-80"
         target="_blank"
         rel="noreferrer"
       >
